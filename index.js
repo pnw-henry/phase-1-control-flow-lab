c73105137356e25711fa815f7bd59d274031eb6d
Henry Escobar <henrye@gmail.com>
2022-05-17 20:56:00 -0700

    commit tree b0b1b7ce5902263d01a62fcbd649320239ba775e

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let charge = 0;
+  if (distance <= 400){
+    charge = 'This one is on me!';
+    return charge;
+  }
+
+  else if (distance > 400 && distance <= 2000){
+    charge = 'That will be twenty bucks.';
+    return charge;
+  }
+
+  else if (distance > 2000 && distance < 2500){
+    charge = 'I will gladly take your thirty bucks.';
+    return charge;
+  }
+
+  else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let drive;
+  drive = (city === 'NYC') ? "Ok, sounds good." : "No go.";
+  return drive;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let message;
+  switch(tip){
+    case 'generous':
+      message = 'Thank you so much.';
+      return message;
+      break;
+    case 'not as generous':
+      message = 'Thank you.';
+      return message;
+      break;
+    default:
+      message = 'Bye.';
+      return message;  
+  }
 }
